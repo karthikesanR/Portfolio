526307abff20b384632b1fbfa93e803b3db17432
Karthikesan <karthikesan92001@gmail.com>
2023-05-02 19:14:58 +0530

    i add some content

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-$(window).on('load scroll', function() {
-    var scrollTop = $(this).scrollTop();
-    var windowHeight = $(this).height();
-    $('.timeline-item').each(function() {
-      var offsetTop = $(this).offset().top;
-      if (scrollTop + windowHeight > offsetTop) {
-        $(this).addClass('animated');
-      }
-    });
-  });
+const btn = document.querySelector('.btn');
+const fullDescription = document.querySelector('#full-description');
+
+btn.addEventListener('click', () => {
+  if (fullDescription.style.display === 'none') {
+    fullDescription.style.display = 'block';
+    btn.textContent = 'Read Less';
+  } else {
+    fullDescription.style.display = 'none';
+    btn.textContent = 'Read More';
+  }
+});
+
   
